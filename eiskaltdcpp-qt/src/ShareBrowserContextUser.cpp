d4fb9549dee5b007cac54fa225cbeee89dbdde0c
Peter M Adrianov <peter.adrianov@gmail.com>
2026-07-13 09:40:06 +0300

    refactor: wire DeleteFile action and guard null directory pointer in ShareBrowser

--- eiskaltdcpp-qt/src/ShareBrowserContextUser.cpp
+++ eiskaltdcpp-qt/src/ShareBrowserContextUser.cpp
@@ -9,6 +9,8 @@
 
 #include "ShareBrowser.h"
 #include "FileBrowserModel.h"
+#include "SearchLocalPath.h"
+#include "WulforUtil.h"
 
 #include "dcpp/FavoriteManager.h"
 #include "dcpp/ClientManager.h"
@@ -20,6 +22,37 @@
 #include <QUrl>
 
 using namespace dcpp;
+
+namespace {
+
+StringList localPaths(DirectoryListing &listing, FileBrowserItem *item)
+{
+    if (!item)
+        return StringList();
+
+    ShareManager *SM = ShareManager::getInstance();
+    try {
+        if (item->file)
+            return listing.getLocalPaths(item->file);
+
+        if (!item->dir)
+            return StringList();
+
+        auto *adl = dynamic_cast<DirectoryListing::AdlDirectory*>(item->dir);
+        if (!adl)
+            return listing.getLocalPaths(item->dir);
+
+        QStringList parts = _q(adl->getFullPath()).split('\\');
+        if (parts.isEmpty())
+            return StringList();
+        parts.removeFirst();
+        return SM->getRealPaths(Util::toAdcFile(_tq(parts.join("\\") + '\\')));
+    } catch (...) {
+        return StringList();
+    }
+}
+
+} // namespace
 
 void ShareBrowser::contextUserActions(Menu::Action act, const QModelIndexList &list)
 {
@@ -55,49 +88,38 @@ void ShareBrowser::contextUserActions(Menu::Action act, const QModelIndexList &l
 
             break;
         }
+        case Menu::OpenFile:
+        {
+            for (const auto &index : list) {
+                FileBrowserItem *item = reinterpret_cast<FileBrowserItem*>(index.internalPointer());
+                if (!item || !item->file)
+                    continue;
+                for (const auto &path : localPaths(listing, item))
+                    SearchLocalPath::openFile(_q(path));
+            }
+            break;
+        }
         case Menu::OpenUrl:
         {
-            ShareManager *SM = ShareManager::getInstance();
-
-            for (const auto &index : list){
+            for (const auto &index : list) {
                 FileBrowserItem *item = reinterpret_cast<FileBrowserItem*>(index.internalPointer());
-
                 if (!item)
                     continue;
 
-                DirectoryListing::AdlDirectory *adl_dir = dynamic_cast<DirectoryListing::AdlDirectory*>(item->dir);
-                dcpp::StringList lst;
-
-                try {
-                    if (!adl_dir)
-                        lst  = listing.getLocalPaths(item->dir);
-                    else{
-                        QStringList path_lst = _q(adl_dir->getFullPath()).split('\\');
-
-                        if (path_lst.size() < 1)
-                            break;
-
-                        path_lst.removeFirst();//remove root element
-
-                        QString path = path_lst.join("\\")+'\\';
-
-                        lst = SM->getRealPaths(Util::toAdcFile(_tq(path)));
-                    }
-                }
-                catch ( ... ){ }
-
-                for (const auto &it : lst){
-                    if (QDir(_q(it)).exists())
-#ifndef Q_OS_WIN
-                        QDesktopServices::openUrl(QUrl("file://"+_q(it)));
-#else
-                        QDesktopServices::openUrl(QUrl("file:///"+_q(it)));
-#endif
+                for (const auto &path : localPaths(listing, item)) {
+                    const QString local = _q(path);
+                    if (item->file)
+                        WulforUtil::revealPath(local);
+                    else if (QDir(local).exists())
+                        QDesktopServices::openUrl(QUrl::fromLocalFile(local));
                 }
             }
 
             break;
         }
+        case Menu::DeleteFile:
+            deleteOwnItems(list);
+            break;
         default: break;
     }
 }
