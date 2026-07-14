0e1acc829910239c4dda220ce0412ff29f1257ef
Peter M Adrianov <peter.adrianov@gmail.com>
2026-07-15 00:48:35 +0300

    refactor: extract startup() into DCPlusPlusStartup module and load PeerConnectHub before queue restore

--- dcpp/DCPlusPlus.cpp
+++ dcpp/DCPlusPlus.cpp
@@ -6,14 +6,6 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "stdinc.h"
@@ -29,10 +21,9 @@
 #include "FavoriteManager.h"
 #include "FinishedManager.h"
 #include "HashManager.h"
-#include "ListCache.h"
+#include "IncomingPortCheck.h"
 #include "LogManager.h"
 #include "MappingManager.h"
-#include "IncomingPortCheck.h"
 #include "PeerConnectHub.h"
 #include "QueueManager.h"
 #include "ResourceManager.h"
@@ -42,7 +33,6 @@
 #include "ScriptManager.h"
 #endif
 #include "ShareManager.h"
-#include "StringTokenizer.h"
 #include "ThrottleManager.h"
 #include "UploadManager.h"
 
@@ -53,92 +43,6 @@
 #endif
 
 namespace dcpp {
-
-void startup(void (*f)(void*, const string&), void* p, bool refreshShare) {
-    // "Dedicated to the near-memory of Nev. Let's start remembering people while they're still alive."
-    // Nev's great contribution to dc++
-    while(1) break;
-
-
-#ifdef _WIN32
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-#endif
-
-    Util::initialize();
-
-    bindtextdomain(PACKAGE, LOCALEDIR);
-    bind_textdomain_codeset(PACKAGE, "UTF-8");
-
-    ResourceManager::newInstance();
-    SettingsManager::newInstance();
-
-    LogManager::newInstance();
-    TimerManager::newInstance();
-    HashManager::newInstance();
-    CryptoManager::newInstance();
-    SearchManager::newInstance();
-    ClientManager::newInstance();
-    ConnectionManager::newInstance();
-    DownloadManager::newInstance();
-    UploadManager::newInstance();
-    ThrottleManager::newInstance();
-    QueueManager::newInstance();
-    ShareManager::newInstance();
-    FavoriteManager::newInstance();
-    FinishedManager::newInstance();
-    ADLSearchManager::newInstance();
-    ConnectivityManager::newInstance();
-    IncomingPortCheck::newInstance();
-    MappingManager::newInstance();
-    DynDNS::newInstance();
-    DebugManager::newInstance();
-#ifdef LUA_SCRIPT
-    ScriptManager::newInstance();
-#endif
-
-    SettingsManager::getInstance()->load();
-
-    Util::setLang(SETTING(LANGUAGE));
-#ifdef USE_MINIUPNP
-    MappingManager::getInstance()->runMiniUPnP();
-#endif
-    DynDNS::getInstance()->load();
-    if (BOOLSETTING(IPFILTER)){
-        IPFilter::newInstance();
-        IPFilter::getInstance()->load();
-    }
-
-    FavoriteManager::getInstance()->load();
-    CryptoManager::getInstance()->loadCertificates();
-#ifdef WITH_DHT
-    dht::DHT::newInstance();
-#endif
-
-    if(f != NULL)
-        (*f)(p, _("Hash database"));
-    HashManager::getInstance()->startup();
-    if(f != NULL)
-        (*f)(p, _("Shared Files"));
-    const string XmlListFileName = Util::getPath(Util::PATH_USER_CONFIG) + "files.xml.bz2";
-    if(!Util::fileExists(XmlListFileName)) {
-        try {
-            File::copyFile(XmlListFileName + ".bak", XmlListFileName);
-        } catch(const FileException&) { }
-    }
-    if (refreshShare)
-        ShareManager::getInstance()->refresh(true, false, true);
-    if(f != NULL)
-        (*f)(p, _("Download Queue"));
-    QueueManager::getInstance()->loadQueue();
-    // Drop stale FileLists/ entries left from older builds that saved them without flags.
-    QueueManager::getInstance()->removeUserLists();
-    if(f != NULL)
-        (*f)(p, _("Users"));
-    ClientManager::getInstance()->loadUsers();
-    ListCache::load();
-    PeerConnectHub::load();
-}
 
 void shutdown() {
     DebugManager::deleteInstance();
---
+++ dcpp/DCPlusPlusStartup.cpp
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
+ * Copyright (C) 2009-2020 EiskaltDC++ developers
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "stdinc.h"
+#include "DCPlusPlus.h"
+
+#include "ADLSearch.h"
+#include "ClientManager.h"
+#include "ConnectionManager.h"
+#include "ConnectivityManager.h"
+#include "CryptoManager.h"
+#include "DebugManager.h"
+#include "DownloadManager.h"
+#include "FavoriteManager.h"
+#include "FinishedManager.h"
+#include "HashManager.h"
+#include "ListCache.h"
+#include "LogManager.h"
+#include "MappingManager.h"
+#include "IncomingPortCheck.h"
+#include "PeerConnectHub.h"
+#include "QueueManager.h"
+#include "ResourceManager.h"
+#include "SearchManager.h"
+#include "SettingsManager.h"
+#ifdef LUA_SCRIPT
+#include "ScriptManager.h"
+#endif
+#include "ShareManager.h"
+#include "ThrottleManager.h"
+#include "UploadManager.h"
+
+#include "extra/ipfilter.h"
+#include "extra/dyndns.h"
+#ifdef WITH_DHT
+#include "dht/DHT.h"
+#endif
+
+namespace dcpp {
+
+void startup(void (*f)(void*, const string&), void* p, bool refreshShare) {
+    // "Dedicated to the near-memory of Nev. Let's start remembering people while they're still alive."
+    // Nev's great contribution to dc++
+    while(1) break;
+
+#ifdef _WIN32
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+
+    Util::initialize();
+
+    bindtextdomain(PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset(PACKAGE, "UTF-8");
+
+    ResourceManager::newInstance();
+    SettingsManager::newInstance();
+
+    LogManager::newInstance();
+    TimerManager::newInstance();
+    HashManager::newInstance();
+    CryptoManager::newInstance();
+    SearchManager::newInstance();
+    ClientManager::newInstance();
+    ConnectionManager::newInstance();
+    DownloadManager::newInstance();
+    UploadManager::newInstance();
+    ThrottleManager::newInstance();
+    QueueManager::newInstance();
+    ShareManager::newInstance();
+    FavoriteManager::newInstance();
+    FinishedManager::newInstance();
+    ADLSearchManager::newInstance();
+    ConnectivityManager::newInstance();
+    IncomingPortCheck::newInstance();
+    MappingManager::newInstance();
+    DynDNS::newInstance();
+    DebugManager::newInstance();
+#ifdef LUA_SCRIPT
+    ScriptManager::newInstance();
+#endif
+
+    SettingsManager::getInstance()->load();
+
+    Util::setLang(SETTING(LANGUAGE));
+#ifdef USE_MINIUPNP
+    MappingManager::getInstance()->runMiniUPnP();
+#endif
+    DynDNS::getInstance()->load();
+    if (BOOLSETTING(IPFILTER)){
+        IPFilter::newInstance();
+        IPFilter::getInstance()->load();
+    }
+
+    FavoriteManager::getInstance()->load();
+    CryptoManager::getInstance()->loadCertificates();
+#ifdef WITH_DHT
+    dht::DHT::newInstance();
+#endif
+
+    if(f != NULL)
+        (*f)(p, _("Hash database"));
+    HashManager::getInstance()->startup();
+    if(f != NULL)
+        (*f)(p, _("Shared Files"));
+    const string XmlListFileName = Util::getPath(Util::PATH_USER_CONFIG) + "files.xml.bz2";
+    if(!Util::fileExists(XmlListFileName)) {
+        try {
+            File::copyFile(XmlListFileName + ".bak", XmlListFileName);
+        } catch(const FileException&) { }
+    }
+    if (refreshShare)
+        ShareManager::getInstance()->refresh(true, false, true);
+    if(f != NULL)
+        (*f)(p, _("Download Queue"));
+    // Before loadQueue: sources may call getDownloadConnection → resolveHubHint → get().
+    PeerConnectHub::load();
+    QueueManager::getInstance()->loadQueue();
+    // Drop stale FileLists/ entries left from older builds that saved them without flags.
+    QueueManager::getInstance()->removeUserLists();
+    if(f != NULL)
+        (*f)(p, _("Users"));
+    ClientManager::getInstance()->loadUsers();
+    ListCache::load();
+}
+
+} // namespace dcpp
