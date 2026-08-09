cddf3fd34dc200918333df6a7ed0b03449c39a20
Peter M Adrianov <peter.adrianov@gmail.com>
2026-08-09 07:22:03 +0300

    build: add development version constants

---
+++ VersionGlobal.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 3 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#pragma once
+
+#include <string>
+
+#define VERSIONSTRING "2.5.0"
+#define VERSIONFLOAT "2.0500"
+#define APPNAME "EiskaltDC++"
+#define DCPP_VERSION "0.785"
+
+const std::string eiskaltdcppVersionString("2.5.0+devel");
+const std::string eiskaltdcppAppNameString("EiskaltDC++");
+
